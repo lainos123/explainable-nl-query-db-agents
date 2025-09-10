ee66dc632742ec58de627c930664955ba05ae269
Cedrus Dang <dtt161094@gmail.com>
2025-09-10 10:14:12 +0800

    Add APIs demo, add backend module folders, setup venv into backend folder (correction)

--- web_app/backend/backend/urls.py
+++ web_app/backend/backend/urls.py
@@ -15,8 +15,33 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
+# TODO: DEMOTRATION ONLY: WILL BE ADDED DURING DEVELOPMENT
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+
+    # API docs and entrypoints
+    # GET /api/docs -> Swagger UI
+    # GET /api/apis -> json of available APIs, endpoints, and parameters
+    path("api/", include("api.urls")),  
+
+    # Database management
+    # POST /data/add -> add one file only
+    # POST /data/add-zip -> add multiple files in a zip
+    # DELETE /data/{id} -> remove
+    # GET /data/list -> list
+    # GET /data/{id}/schema -> schema
+    # GET /data/download/{id} -> download original file
+    # GET /data/download-zip -> download all files in a zip
+    # GET /data/query?{sql-query} -> SQL query
+    path("data/", include("db.urls")),
+
+    # Agent query
+    # GET /agent/a
+    # GET /agent/a?{actions} # last, change-last, add-last, remove-last, delete-all, receive-all
+    # GET /agent/b
+    # GET /agent/c
+    # GET /agent/d
+    path("agent/", include("query.urls")),
 ]
