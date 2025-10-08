f9c0dd1d51bafdd238bbde1a14f7e16d5a67b272
Cedrus Dang <dtt161094@gmail.com>
2025-10-09 05:57:23 +0800

    Move FE port to 3001 for safer port

--- web_app/backend/config/settings.py
+++ web_app/backend/config/settings.py
@@ -7,8 +7,8 @@ from datetime import timedelta
 
 # CORS config, Please adjust according to your frontend domains, very important !!!
 CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:3000",
-    "http://localhost:3000",
+    "http://127.0.0.1:3001",
+    "http://localhost:3001",
     "http://127.0.0.1:8000",
     "http://localhost:8000",
 ]
